b32242a9090ff096508d183f983730a2baf4b454
machinegirl <machinegirl@eternalvoid.net>
2019-03-17 17:15:17 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+(() => {
+
+  function getBlogPostsConf() {
+    return fetch('blog-posts.conf')
+    .then(res => {
+      return res.text();
+    })
+    .then(res => {
+      return(res.split('\n'));
+    });
+  
+  }
+
+  function makePostsHtml(posts) {
+    return posts.map(v => {
+      return '<div class="blog-post">' + marked(v) + '</div>'
+    }).join('\n');
+  }
+
+  let md = document.getElementById('markdown-area');
+  getBlogPostsConf()
+  .then(fileNames => {
+    Promise.all(fileNames.map(f => fetch('/blog-posts/' + f)))
+    .then(responses => Promise.all(responses.map(res => res.text()))
+    ).then(posts => {
+      md.innerHTML = makePostsHtml(posts);
+    });
+  });
+
+})();
